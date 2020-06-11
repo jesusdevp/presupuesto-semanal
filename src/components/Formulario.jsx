85c6f9524278d271c2f9d9eedb0e5f5c7013b16e
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-11 16:04:53 -0500

    State para gastos y validacion

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,59 @@
 import React, { useState } from "react";
+import Error from "./Error";
+import shortid from "shortid";
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState(0);
+  const [error, setError] = useState(false);
+
+  //Agregar gasto
+
+  const agregarGasto = (e) => {
+    e.preventDefault();
+
+    //validar
+    if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    const gasto = {
+      nombre,
+      cantidad,
+      id: shortid.generate(),
+    };
+
+    console.log(gasto);
+  };
+
   return (
-    <form>
+    <form onSubmit={agregarGasto}>
       <h2>Agrega tus gastos aqui</h2>
+      {error ? (
+        <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" />
+      ) : null}
       <div className="campo">
         <label>Nombre gasto</label>
-        <input type="text" className="u-full-width" placeholder="Ej. Comida" />
+        <input
+          type="text"
+          className="u-full-width"
+          placeholder="Ej. Comida"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
       </div>
       <div className="campo">
         <label>Cantidad Gasto</label>
-        <input type="number" className="u-full-width" placeholder="Ej. 300" />
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Ej. 300"
+          value={cantidad}
+          onChange={(e) => setCantidad(parseInt(e.target.value))}
+        />
       </div>
       <input
         type="submit"
